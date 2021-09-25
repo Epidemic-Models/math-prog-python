f5b3b7f75a6aaf369be26b3a0d2a676c032321c8
shon19 <78174798+shon19@users.noreply.github.com>
2021-09-25 21:52:13 +0200

    Implement convert method for BoundingBoxes

--- bounding_box_a.py
+++ bounding_box_a.py
@@ -3,25 +3,23 @@ from bounding_box import BoundingBox
 
 
 class BoundingBoxA(Rectangle):
-    def __init__(self, left_upper: tuple, right_lower: tuple, width: float, height: float):
-        super().__init__(width, height)
+    def __init__(self, left_upper: tuple, right_lower: tuple):
         self.left_upper = left_upper
         self.right_lower = right_lower
         width = self.right_lower[0] - self.left_upper[0]
         height = self.left_upper[1] - self.right_lower[1]
-        self.width = width
-        self.height = height
+        super().__init__(width=width, height=height)
 
     def __str__(self):
         return "BoundingBoxA: left_upper=" + str(self.left_upper) +\
                ", right_lower=" + str(self.right_lower)
 
     def convert(self):
-        return BoundingBox(left_upper=self.left_upper, width=self.width, height=self.height)
+        return BoundingBox(self.width, self.height, self.left_upper)
 
 
 def main():
-    bounding_box_a = BoundingBoxA(left_upper=(2, 3), right_lower=(5, 1), height=True, width=True)
+    bounding_box_a = BoundingBoxA(left_upper=(2, 3), right_lower=(5, 1))
     print(bounding_box_a)
     print(bounding_box_a.convert())
 
