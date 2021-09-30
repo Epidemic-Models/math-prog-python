ed43efbf8940f1beacc1178e40718772bdd163f5
dbornali <das.bornali19@gmail.com>
2021-09-30 18:12:55 +0200

    Annotate return Values in bounding_box.py

--- bounding_box.py
+++ bounding_box.py
@@ -2,16 +2,16 @@ from rectangle import Rectangle
 
 
 class BoundingBox(Rectangle):
-    def __init__(self, width: float, height: float, left_upper: tuple):
+    def __init__(self, width: float, height: float, left_upper: tuple) -> None:
         super().__init__(width=width, height=height)
         self.left_upper = left_upper
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "BoundingBox: left_upper=" + str(self.left_upper) + \
                ", width=" + str(self.width) + ", height=" + str(self.height)
 
 
-def main():
+def main() -> None:
     bounding_box = BoundingBox(width=2.0, height=3.0, left_upper=(2.3, 4))
     print(bounding_box)
 
