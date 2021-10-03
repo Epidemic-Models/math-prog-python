e4b20e8dde5645958aaca3a5e314e2255f064511
dbornali <das.bornali19@gmail.com>
2021-10-03 10:36:20 +0200

    Refactor bounding_box_a.py

--- bounding_box_b.py
+++ bounding_box_b.py
@@ -67,14 +67,6 @@ class BoundingBoxB(Rectangle):
         return intersection
 
 
-def main() -> None:
-    bounding_box_1 = BoundingBoxB(left_lower=Point(p=(2.1, 2)), right_upper=Point(p=(3, 3)))
-    print(bounding_box_1)
-    print(bounding_box_1.convert())
-    bounding_box_2 = BoundingBoxB(left_lower=Point(p=(2, 2)), right_upper=Point(p=(4, 4)))
-    print("A different representation of the bounding box: ", bounding_box_1.convert())
-    print("The union of the two bounding boxes: ", bounding_box_1.get_union(bounding_box_2))
-    print("The intersection of the two bounding boxes: ", bounding_box_1.get_intersection(bounding_box_2))
 
 
 if __name__ == "__main__":
