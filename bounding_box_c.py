7af4ccf5b031f5ceeb0be037c3fe393d60e9564e
dbornali <das.bornali19@gmail.com>
2021-10-03 13:44:25 +0200

    Refactor bounding_box_c.py

--- bounding_box_c.py
+++ bounding_box_c.py
@@ -11,6 +11,7 @@ class BoundingBoxC(Rectangle):
         super().__init__(width=width, height=height)
         self.__left_lower = None
         self.__right_upper = None
+        self.__right_lower = None
         self.__centre = None
 
     def __str__(self) -> str:
@@ -37,7 +38,13 @@ class BoundingBoxC(Rectangle):
     def right_upper(self) -> Point:
         shift = Point(p=(self.width, 0))
         self.__right_upper = self.left_upper + shift
-        return self.__left_lower
+        return self.__right_upper
+
+    @property
+    def right_lower(self) -> Point:
+        shift = Point(p=(self.width, - self.height))
+        self.__right_lower = self.left_upper + shift
+        return self.__right_lower
 
     @property
     def centre(self) -> Point:
@@ -45,17 +52,19 @@ class BoundingBoxC(Rectangle):
         self.__centre = self.left_upper + shift
         return self.__centre
 
-    def get_union(self, other: BoundingBoxA) -> BoundingBoxA:
+    def get_union(self, other: BoundingBoxC) -> BoundingBoxC:
         union_left_upper = Point(p=(min(self.left_upper.x, other.left_upper.x),
                                     max(self.left_upper.y, other.left_upper.y))
                                  )
         union_right_lower = Point(p=(max(self.right_lower.x, other.right_lower.x),
                                      min(self.right_lower.y, other.right_lower.y))
                                   )
-        union = BoundingBoxA(left_upper=union_left_upper, right_lower=union_right_lower)
+        union_height = union_left_upper.y - union_right_lower.y
+        union_width = union_right_lower.x - union_left_upper.x
+        union = BoundingBoxC(width=union_width, height=union_height, left_upper=union_left_upper)
         return union
 
-    def get_intersection(self, other: BoundingBoxA) -> BoundingBoxA:
+    def get_intersection(self, other: BoundingBoxC) -> BoundingBoxC:
         intersection_left_upper = Point(p=(max(self.left_upper.x, other.left_upper.x),
                                            min(self.left_upper.y, other.left_upper.y))
                                         )
@@ -67,7 +76,20 @@ class BoundingBoxC(Rectangle):
 
         intersection = None
         if intersection_height > 0 and intersection_width > 0:
-            intersection = BoundingBoxA(left_upper=intersection_left_upper,
-                                        right_lower=intersection_right_lower)
+            intersection = BoundingBoxC(width=intersection_width, height=intersection_height,
+                                        left_upper=intersection_left_upper)
 
         return intersection
+
+
+def main() -> None:
+    bounding_box_c = BoundingBoxC(width=2, height=8, left_upper=Point(p=(2, 7)))
+    bounding_box_other = BoundingBoxC(width=2, height=6, left_upper=Point(p=(1, 5)))
+    print(bounding_box_c)
+    print("A different representation of the bounding box: ", bounding_box_c.convert())
+    print("The intersection of the two bounding boxes: ", bounding_box_c.get_intersection(bounding_box_other))
+    print("The union of the two bounding boxes: ", bounding_box_c.get_union(bounding_box_other))
+
+
+if __name__ == "__main__":
+    main()
