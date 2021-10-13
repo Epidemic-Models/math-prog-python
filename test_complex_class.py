16cd798d67e03adfe7d6d594a829816ed8f31d11
bornalid <das.bornali19@gmail.com>
2021-10-13 14:53:17 +0200

    test_bounding_box.py

--- test_complex_class.py
+++ test_complex_class.py
@@ -9,14 +9,14 @@ def main() -> None:
     complex_b = Complex(real=-2, imaginary=-5)
     print("complex_b = " + str(complex_b))
     print("Conjugate of b = " + str(complex_b.conjugate()))
-    print("complex_a == complex_b is " + str(complex_a.__eq__(complex_b)))
-    print("complex_a != complex_b is " + str(complex_a.__ne__(complex_b)))
-    print("complex_a + complex_b = " + str(complex_a.__add__(complex_b)))
-    print("complex_a - complex_b = " + str(complex_a.__sub__(complex_b)))
-    print("complex_a * complex_b = " + str(complex_a.__mul__(complex_b)))
-    print("complex_a / complex_b = " + str(complex_a.__truediv__(complex_b)))
+    print("complex_a == complex_b is " + str(complex_a == complex_b))
+    print("complex_a != complex_b is " + str(complex_a != complex_b))
+    print("complex_a + complex_b = " + str(complex_a+complex_b))
+    print("complex_a - complex_b = " + str(complex_a-complex_b))
+    print("complex_a * complex_b = " + str(complex_a*complex_b))
+    print("complex_a / complex_b = " + str(complex_a/complex_b))
     n = 5
-    print("complex_a ** 5 = " + str(complex_a.__pow__(n)))
+    print("complex_a ** 5 = " + str(complex_a**n))
 
 
 if __name__ == '__main__':
